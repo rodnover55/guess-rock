3c7cc5ea902f44f2c515990b156563cfdfb19220
Roman Dubrovin <ozkedko@gmail.com>
2016-01-05 15:31:32 +0600

    vendors updated

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -13,6 +13,9 @@ return array(
     $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
     $vendorDir . '/psy/psysh/src/Psy/functions.php',
     $vendorDir . '/paragonie/random_compat/lib/random.php',
+    $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
+    $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
+    $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
 );
